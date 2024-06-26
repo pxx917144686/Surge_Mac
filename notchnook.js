6c7ba8079886f8386d64a68215145193d44bd4df
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-06-27 04:13:41 +0800

    Create notchnook.js

---
+++ notchnook.js
@@ -0,0 +1,30 @@
+/*
+[rewrite_local]
+# pxx917144686
+# 此脚本用于 notchnook 的破解 官方网站： https://lo.cafe/notchnook
+
+^https:\/\/lo\.cafe\/api\/notchnook-verify-key$ url script-response-body https://raw.githubusercontent.com/pxx917144686/pxx/main/notchnook.js
+
+[mitm]
+hostname = lo.cafe
+
+const jwt = require('jsonwebtoken');
+
+var response = {
+  valid: false,
+  iat: Math.floor(Date.now() / 1000),
+};
+
+const token = jwt.sign(response, 'secret', { algorithm: 'RS256' });
+
+var responseBody = {
+  token: token,
+};
+
+$done({
+  status: 200,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify(responseBody)
+});
