2e0ffdab89a3783674b1cf9361ca3d60635f79e6
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2024-06-27 05:07:29 +0800

    Update notchnook.js

--- notchnook.js
+++ notchnook.js
@@ -3,12 +3,12 @@
 # notchnook 破解
 # 此脚本用于 notchnook 的破解
 
-^https:\/\/api\.lemonsqueezy\.com\/v1\/licenses url script-response-body https://github.com/wibus-wee/activation-script/raw/gh-pages/notchnook.js
-^https:\/\/v3\.paddleapi\.com\/3\.2\/license url script-response-body https://github.com/wibus-wee/activation-script/raw/gh-pages/notchnook.js
-^https:\/\/api\.gumroad\.com\/v2\/licenses url script-response-body https://github.com/wibus-wee/activation-script/raw/gh-pages/notchnook.js
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://github.com/wibus-wee/activation-script/raw/gh-pages/notchnook.js
-^https:\/\/backend\.raycast\.com\/api\/v1\/me url script-response-body https://github.com/wibus-wee/activation-script/raw/gh-pages/notchnook.js
-^https:\/\/shottr\.cc\/licensing\/verify\.php url script-response-body https://github.com/wibus-wee/activation-script/raw/gh-pages/notchnook.js
+^https:\/\/api\.lemonsqueezy\.com\/v1\/licenses url script-response-body https://raw.githubusercontent.com/pxx917144686/pxx/main/notchnook.js
+^https:\/\/v3\.paddleapi\.com\/3\.2\/license url script-response-body https://raw.githubusercontent.com/pxx917144686/pxx/main/notchnook.js
+^https:\/\/api\.gumroad\.com\/v2\/licenses url script-response-body https://raw.githubusercontent.com/pxx917144686/pxx/main/notchnook.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/pxx917144686/pxx/main/notchnook.js
+^https:\/\/backend\.raycast\.com\/api\/v1\/me url script-response-body https://raw.githubusercontent.com/pxx917144686/pxx/main/notchnook.js
+^https:\/\/shottr\.cc\/licensing\/verify\.php url script-response-body https://raw.githubusercontent.com/pxx917144686/pxx/main/notchnook.js
 
 [mitm]
 hostname = api.lemonsqueezy.com, v3.paddleapi.com, api.gumroad.com, buy.itunes.apple.com, backend.raycast.com, shottr.cc
